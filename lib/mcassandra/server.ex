79f162a5fcf8ea4c896151360f536e2e385fc65c
Muaaz Saleem <muaazsaleem@outlook.com>
2017-01-19 23:12:50 +0100

    Added both GenServer and Agent based implementations of Client
    Signed-off-by: Muaaz Saleem <muaazsaleem@outlook.com>

--- lib/mcassandra/server.ex
+++ lib/mcassandra/server.ex
@@ -7,6 +7,7 @@ defmodule Mcassandra.Server do
     GenServer.start_link(__MODULE__, {%{}, %{}})
   end
 
+
   @doc """
   Will create Agents for each of the columns in the schema
   :table_name -> :primary_key -> [:column1, :column2] -> :ok|:error
@@ -16,9 +17,6 @@ defmodule Mcassandra.Server do
 
   """
   def create_table(pid, table_name, primary_key, columns) do
-    IO.puts "Creating table:#{table_name} with primary_key:#{primary_key} \
-     with columns: #{Enum.join columns, ", "}"
-
     GenServer.call(pid, {:new_table, table_name, primary_key, columns})
   end
 
@@ -26,41 +24,61 @@ defmodule Mcassandra.Server do
   Will insert each column into respective Agents as {primany_key, column_values}
   """
   def insert(pid, table_name, primary_key, columns) do
-    IO.puts "Inserting columns: #{Enum.join columns, ", "}
-    with primary_key: #{primary_key}
-    in table: #{table_name}"
     GenServer.call(pid, {:insert, table_name, primary_key, columns})
   end
 
-  # Server API
+  @doc """
+  Get a single column value from a table
+  """
+  def get_column(pid, table_name, primary_key, column) do
+    GenServer.call(pid, {:get_column, table_name, primary_key, column})
+  end
+
+  @doc """
+  Get a range of column values by primary_key from a table
+  """
+  def get_column_range(pid, table_name, primary_key_range, column) do
+    GenServer.call(pid, {:get_column_range, table_name, primary_key_range, column})
+  end
+
+  ### Server API
 
   def handle_call({:new_table, table_name, _primary_key, columns}, _from, {format, data}) do
     new_format = Map.put_new(format, table_name, columns)
-
-    data_cells =  Enum.reduce(columns,
-                  %{},
+    data_cells =  Enum.reduce(columns, %{},
                   fn column, columns ->
                     Map.put_new(columns, column, %{})
                   end)
-
     new_data = Map.put_new(data, table_name, data_cells)
-
-    {:reply, :ok, {new_format, new_data}}
+    {:reply, {:ok, table_name}, {new_format, new_data}}
   end
 
   def handle_call({:insert, table_name, primary_key, column_values}, _from, {format, data}) do
-      columns = Map.get(format, table_name)
-
-      data_cells = Map.get(data, table_name)
-
-      new_data = Enum.zip(columns, column_values)
-        |> Map.new
-        |> Enum.reduce(data_cells,
-          fn {column, value}, data_cells ->
-            Map.put(data_cells, column, %{primary_key => value})
-          end)
-
-      {:reply, :ok, {format, new_data}}
+    columns = Map.get(format, table_name)
+    table = Map.get(data, table_name)
+    updated_table =
+      Enum.zip(columns, column_values)
+      |> Map.new
+      |> Enum.reduce(table, fn {column, value}, table ->
+          updated_column = Map.get(table, column)
+            |> Map.put(primary_key, value)
+          Map.put(table, column, updated_column)
+        end)
+    state = Map.put(data, table_name, updated_table)
+    {:reply, {:ok, primary_key}, {format, state}}
   end
 
+  def handle_call({:get_column, table_name, primary_key, column}, _from, {format, data}) do
+    value = Map.get(data, table_name)
+      |> Map.get(column)
+      |> Map.get(primary_key)
+    {:reply, {:ok, value}, {format, data}}
+  end
+
+  def handle_call({:get_column_range, table_name, primary_key_range, column}, _from, {format, data}) do
+    column = Map.get(data, table_name)
+      |> Map.get(column)
+    values = Enum.map(primary_key_range, fn primary_key -> Map.get(column, primary_key) end)
+    {:reply, {:ok, values}, {format, data}}
+  end
 end
--- test/mcassandra_test.exs
+++ test/mcassandra_test.exs
@@ -1,8 +1,3 @@
 defmodule McassandraTest do
   use ExUnit.Case
-  doctest Mcassandra
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
